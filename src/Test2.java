35df45612b3ae71b3252f93519a410d1803a819d
hp <hp@LAPTOP-JB4E9LN0>
2023-10-15 09:49:36 +0530

    This is my Second Commit

---
+++ src/Test2.java
@@ -0,0 +1,12 @@
+
+public class Test2 {
+	public static void main(String[] args) {
+		
+		String name = "Surya";
+		
+		if (name == "Surya")
+			System.out.println("Yeah correct");
+		else
+			System.out.println("Sorry mistake");
+	}
+}
