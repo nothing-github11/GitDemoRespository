e4658660db0e60051f224db6058cc30066a98ab4
hp <hp@LAPTOP-JB4E9LN0>
2023-10-15 09:05:23 +0530

    This is my First Commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+	public static void main(String[] args) {
+		
+		String name = "sai krishna";
+		
+		if (name == "sai krishna")
+			System.out.println("Yeah correct");
+		else
+			System.out.println("Sorry mistake");
+	}
+}
